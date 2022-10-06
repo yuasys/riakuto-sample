599d0faa39bc9848dd99aee1adc28bcf03303ec2
Nobhiko Yuasa <yuasys@gmail.com>
2022-10-06 18:08:18 +0900

    Add get-user-await.js

---
+++ 04-async/get-user-await.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+
+const getUser = async(userID) => {
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${userID}`,
+    );
+
+    if(!response.ok) {
+        throw new Error(`${response.status} Error`);
+    }
+
+    return response.json();
+};
+
+console.log('--- Start ---');
+
+const main = async () => {
+    try {
+        const user = await getUser(2);
+        console.log(user);
+    } catch(error) {
+        console.error(error);
+    } finally {
+        console.log('--- Completed ---');
+    }
+};
+
+main();
