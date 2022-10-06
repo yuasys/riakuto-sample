bd6abcd9387841f369eb556f74252138c3467d6b
Nobhiko Yuasa <yuasys@gmail.com>
2022-10-06 17:33:01 +0900

    Add get-user-then.js

---
+++ 04-async/get-user-then.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+const getUser = (userID) =>
+    fetch(`https://jsonplaceholder.typecode.com/users/${userID}`)
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error(`${response.status} Error`);
+            } else {
+                return response.json();
+            }
+        },
+    );
+
+console.log('--- Start ---');
+
+getUser(2)
+    .then((user) => {
+        console.log(user);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+    .finally(() => {
+        console.log('--- Completed ===')
+    });
+    
