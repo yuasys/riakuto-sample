d9003a2116da73a7bf5c1583747350239c369e40
Nobhiko Yuasa <yuasys@gmail.com>
2022-10-06 23:35:29 +0900

    Add index-signature.ts

---
+++ 02-basic-types/index-signature.ts
@@ -0,0 +1,12 @@
+interface Status {
+    [parameter: string]: number;
+}
+
+const myStatus: Status = {
+    level: 22,
+    experience: 3058,
+    maxHP: 156,
+    maxNP: 174,
+    attack: 39,
+    defense: 25,
+};
