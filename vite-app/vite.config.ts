7bd870eb6b26f7d96ec137d3fd3f25521cf9633e
Nobhiko Yuasa <yuasys@gmail.com>
2022-10-09 17:51:13 +0900

    Update config and json file

--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite'
 // Viteで動くReact用の公式プラグイン
 import react from '@vitejs/plugin-react'
 // React用の他に Vue用（@vitejs/plugin-vue)、Svelte用（@svelte/vite-plugin-svelte）
-// 
+
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),
+    tsconfigPaths()
+  ]
 })
